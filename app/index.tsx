6c474895beeaab1935606d408ed20ba1d1a70656
sa18576 <samikshazamde18576@gmail.com>
2026-02-17 04:18:04 +0530

    env file setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  
+
+  useEffect(() => {
+    fetch('')
+  },[])
   return (
     <View
       style={{
@@ -9,6 +15,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
---
+++ env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+  export const API_READ_ACCESS_TOKEN: string;
+  export const API_KEY: string;
+  // add all your env variables here
+}
